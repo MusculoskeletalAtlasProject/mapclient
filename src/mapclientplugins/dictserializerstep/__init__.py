507630eb491c87200618de789134e98c3d3e9d9f
Hugh Sorby <h.sorby@auckland.ac.nz>
2015-07-21 15:53:25 +1200

    Add some more default steps.

---
+++ src/mapclientplugins/dictserializerstep/__init__.py
@@ -0,0 +1,14 @@
+
+'''
+MAP Client Plugin
+'''
+
+__version__ = '0.1.0'
+__author__ = 'Hugh Sorby'
+__stepname__ = 'Dict Serializer'
+__location__ = 'https://github.com/mapclient-plugins/dictserializer/archive/master.zip'
+
+# import class that derives itself from the step mountpoint.
+from mapclientplugins.dictserializerstep import step
+
+from . import resources_rc
---
+++ src/mapclientplugins/directorychooserstep/step.py
@@ -0,0 +1,106 @@
+
+'''
+MAP Client Plugin Step
+'''
+import json
+
+from PySide import QtGui
+
+from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
+from mapclientplugins.directorychooserstep.configuredialog import ConfigureDialog
+
+
+class DirectoryChooserStep(WorkflowStepMountPoint):
+    '''
+    Skeleton step which is intended to be a helpful starting point
+    for new steps.
+    '''
+
+    def __init__(self, location):
+        super(DirectoryChooserStep, self).__init__('Directory Chooser', location)
+        self._configured = False  # A step cannot be executed until it has been configured.
+        self._category = 'Source'
+        # Add any other initialisation code here:
+        self._icon = QtGui.QImage(':/directorychooserstep/images/data-source.png')
+        # Ports:
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#provides',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#directory_location'))
+        # Port data:
+        self._portData0 = None  # http://physiomeproject.org/workflow/1.0/rdf-schema#directory_location
+        # Config:
+        self._config = {}
+        self._config['identifier'] = ''
+        self._config['Directory'] = ''
+
+
+    def execute(self):
+        '''
+        Add your code here that will kick off the execution of the step.
+        Make sure you call the _doneExecution() method when finished.  This method
+        may be connected up to a button in a widget for example.
+        '''
+        # Put your execute step code here before calling the '_doneExecution' method.
+        self._doneExecution()
+
+    def getPortData(self, index):
+        '''
+        Add your code here that will return the appropriate objects for this step.
+        The index is the index of the port in the port list.  If there is only one
+        provides port for this step then the index can be ignored.
+        '''
+        return self._config['Directory']  # http://physiomeproject.org/workflow/1.0/rdf-schema#directory_location
+
+    def configure(self):
+        '''
+        This function will be called when the configure icon on the step is
+        clicked.  It is appropriate to display a configuration dialog at this
+        time.  If the conditions for the configuration of this step are complete
+        then set:
+            self._configured = True
+        '''
+        dlg = ConfigureDialog()
+        dlg.identifierOccursCount = self._identifierOccursCount
+        dlg.setConfig(self._config)
+        dlg.validate()
+        dlg.setModal(True)
+
+        if dlg.exec_():
+            self._config = dlg.getConfig()
+
+        self._configured = dlg.validate()
+        self._configuredObserver()
+
+    def getIdentifier(self):
+        '''
+        The identifier is a string that must be unique within a workflow.
+        '''
+        return self._config['identifier']
+
+    def setIdentifier(self, identifier):
+        '''
+        The framework will set the identifier for this step when it is loaded.
+        '''
+        self._config['identifier'] = identifier
+
+    def serialize(self):
+        '''
+        Add code to serialize this step to string.  This method should
+        implement the opposite of 'deserialize'.
+        '''
+        return json.dumps(self._config, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+
+    def deserialize(self, string):
+        '''
+        Add code to deserialize this step from string.  This method should
+        implement the opposite of 'serialize'.
+        '''
+        self._config.update(json.loads(string))
+
+        d = ConfigureDialog()
+        d.identifierOccursCount = self._identifierOccursCount
+        d.setConfig(self._config)
+        self._configured = d.validate()
+
+
