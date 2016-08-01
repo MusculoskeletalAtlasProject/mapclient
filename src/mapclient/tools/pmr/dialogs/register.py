b3333026819978e862cdd7fa6bdb73f86dabd949
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-02 10:36:28 +1200

    Small bug fixes and adjustments.

--- src/mapclient/tools/pmr/dialogs/register.py
+++ src/mapclient/tools/pmr/dialogs/register.py
@@ -1,8 +1,22 @@
-'''
-Created on Oct 20, 2015
+"""
+MAP Client, a program to generate detailed musculoskeletal models for OpenSim.
+    Copyright (C) 2012  University of Auckland
 
-@author: hsorby
-'''
+This file is part of MAP Client. (http://launchpad.net/mapclient)
+
+    MAP Client is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MAP Client is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MAP Client.  If not, see <http://www.gnu.org/licenses/>..
+"""
 from PySide import QtGui
 
 from mapclient.tools.pmr.settings.general import PMR
@@ -13,13 +27,13 @@ from mapclient.tools.pmr.dialogs.ui_registerdialog import Ui_RegisterDialog
 
 class PMRRegisterDialog(QtGui.QDialog):
 
-    def __init__(self, parent=None):
+    def __init__(self, external_git, parent=None):
         super(PMRRegisterDialog, self).__init__(parent)
         self._ui = Ui_RegisterDialog()
         self._ui.setupUi(self)
 
         pmr_info = PMR()
-        self._pmr_tool = PMRTool(pmr_info)
+        self._pmr_tool = PMRTool(pmr_info, external_git)
 
         self._makeConnections()
 
