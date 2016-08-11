3696a09b4dddb241c8dcb06c817c75ef3fb7b49a
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-11 14:21:44 +1200

    Set year and copyright holder to author in step license.

--- src/mapclient/tools/pluginwizard/skeletonstrings.py
+++ src/mapclient/tools/pluginwizard/skeletonstrings.py
@@ -375,7 +375,7 @@ __import__('pkg_resources').declare_namespace(__name__)
 
 
 APACHE_LICENSE = """\
-Apache License
+                                Apache License
                            Version 2.0, January 2004
                         http://www.apache.org/licenses/
 
@@ -555,7 +555,7 @@ Apache License
    APPENDIX: How to apply the Apache License to your work.
 
       To apply the Apache License to your work, attach the following
-      boilerplate notice, with the fields enclosed by brackets "{}"
+      boilerplate notice, with the fields enclosed by brackets "{{}}"
       replaced with your own identifying information. (Don't include
       the brackets!)  The text should be enclosed in the appropriate
       comment syntax for the file format. We also recommend that a
@@ -563,7 +563,7 @@ Apache License
       same "printed page" as the copyright notice for easier
       identification within third-party archives.
 
-   Copyright {yyyy} {name of copyright owner}
+   Copyright {yyyy} {name_of_copyright_owner}
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
