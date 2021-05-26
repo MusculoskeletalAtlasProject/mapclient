d7d0b5421b8d7550e6fa6c00900b7beac64ad9bd
hsorby <h.sorby@auckland.ac.nz>
2021-05-27 11:52:12 +1200

    Correct setup for plugin wizard to use long_description_content_type.

--- res/pyinstaller/hooks/hook-mapclientplugins.py
+++ res/pyinstaller/hooks/hook-mapclientplugins.py
@@ -1,4 +1,17 @@
-from PyInstaller.utils.hooks import collect_data_files
+import os
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules, get_package_paths
 
+import mapclientplugins
+
+print("==================================")
+print(mapclientplugins.__path__)
+# hidden = []
+# for p in mapclientplugins.__path__:
+#     d = os.path.dirname(p)
+# print(collect_submodules('mapclientplugins'))
+# print(get_package_paths('mapclientplugins'))
+# print(collect_data_files('mapclientplugins', include_py_files=True, excludes=['__pycache__']))
+print("==================================")
 datas = collect_data_files('mapclientplugins', include_py_files=True, excludes=['__pycache__'])
-hiddenimports = ['imghdr']
+# hiddenimports = ['imghdr', 'mapclientplugins', 'mapclientplugins.datatrimmerstep']
+# hiddenimports = (['mapclientplugins'])
