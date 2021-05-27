b2e5c80fb1472e7ca2b99615f0eab1f69c92fbff
hsorby <h.sorby@auckland.ac.nz>
2021-05-28 08:57:53 +1200

    Add name variant to macOS create installer script.

--- res/macos/create_installer.sh
+++ res/macos/create_installer.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 version=0.15.0
-test -f MAP-Client-$version.dmg && rm MAP-Client-$version.dmg
+variant=-mapping-tools
+
+app_name=MAP-Client$variant-$version
+dmg_name=$app_name.dmg
+test -f $dmg_name && rm $dmg_name
 #  --volicon "application_icon.icns" \
 create-dmg \
-  --volname "MAP-Client-$version" \
+  --volname $app_name \
+  --volicon "MAP-Client.icns" \
   --background "map-client-dmg-background.png" \
   --window-pos 200 120 \
   --window-size 800 400 \
@@ -11,6 +16,6 @@ create-dmg \
   --icon "MAP-Client.app" 200 190 \
   --hide-extension "MAP-Client.app" \
   --app-drop-link 600 185 \
-  "MAP-Client-$version.dmg" \
+  $dmg_name \
   "../pyinstaller/dist/"
 
