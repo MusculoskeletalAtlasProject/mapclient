52b01622e584e8c1b432bec89c062687c085efa0
hsorby <h.sorby@auckland.ac.nz>
2021-11-14 15:45:48 +1300

    Add back in hidden import of mapclientplugins to PyInstaller app creation.

--- res/pyinstaller/create_application.py
+++ res/pyinstaller/create_application.py
@@ -28,7 +28,7 @@ def main(variant):
         '--hidden-import', 'scipy',
         '--hidden-import', 'scipy.interpolate',
         '--hidden-import', 'numpy',
-        # '--hidden-import', 'mapclientplugins',
+        '--hidden-import', 'mapclientplugins',
         # '--hidden-import', 'opencmiss.utils',
         # '--hidden-import', 'opencmiss.zincwidgets',
         '--hidden-import', 'opencmiss.zinc',
