1a679ba36f7903b712aa8f4749b3983f36a2c31d
TimmyNic <tim.nicolas@outlook.com>
2022-09-23 14:44:16 +1200

    Update MessageBox class documentation.

--- src/mapclient/view/dialogs/selfclosing/messagebox.py
+++ src/mapclient/view/dialogs/selfclosing/messagebox.py
@@ -6,7 +6,8 @@ class MessageBox(QtWidgets.QMessageBox):
     def __init__(self, icon, title, text, buttons, parent=None, close_after=None):
         """
         An extension of the Qt.QMessageBox class that automatically closes itself after a set number of seconds. This class allows the user
-        to pass an additional argument (close_after) to specify how long the MessageBox should be displayed for before it terminates.
+        to pass an additional argument (close_after) to specify how long the MessageBox should be displayed for before it terminates. If
+        the close_after argument is set to zero, the MessageBox will not be displayed.
 
         Parameters
         ----------
@@ -16,8 +17,6 @@ class MessageBox(QtWidgets.QMessageBox):
         buttons: PySide2.QtWidgets.QMessageBox.StandardButtons
         parent: PySide2.QtWidgets.QWidget, optional
         close_after: float, optional
-            Specifies the number of seconds that the MessageBox should be displayed for before it closes. If the close_after argument is
-            set to zero, the MessageBox will not be displayed.
         """
         super().__init__(icon, title, text, buttons, parent=parent)
         self._close_after = close_after * 1000
