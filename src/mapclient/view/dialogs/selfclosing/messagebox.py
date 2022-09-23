234694d2edecd21befdd763c84cf448aea0d9333
TimmyNic <tim.nicolas@outlook.com>
2022-09-23 14:20:24 +1200

    Add docstring to MessageBox class.
    Adds a docstring to the MessageBox class.

--- src/mapclient/view/dialogs/selfclosing/messagebox.py
+++ src/mapclient/view/dialogs/selfclosing/messagebox.py
@@ -4,6 +4,21 @@ from PySide2 import QtWidgets, QtCore
 
 class MessageBox(QtWidgets.QMessageBox):
     def __init__(self, icon, title, text, buttons, parent=None, close_after=None):
+        """
+        An extension of the Qt.QMessageBox class that automatically closes itself after a set number of seconds. This class allows the user
+        to pass an additional argument (close_after) to specify how long the MessageBox should be displayed for before it terminates.
+
+        Parameters
+        ----------
+        icon: PySide2.QtWidgets.QMessageBox.Icon
+        title: str
+        text: str
+        buttons: PySide2.QtWidgets.QMessageBox.StandardButtons
+        parent: PySide2.QtWidgets.QWidget, optional
+        close_after: float, optional
+            Specifies the number of seconds that the MessageBox should be displayed for before it closes. If the close_after argument is
+            set to zero, the MessageBox will not be displayed.
+        """
         super().__init__(icon, title, text, buttons, parent=parent)
         self._close_after = close_after * 1000
         QtCore.QTimer.singleShot(self._close_after, self.accept)
