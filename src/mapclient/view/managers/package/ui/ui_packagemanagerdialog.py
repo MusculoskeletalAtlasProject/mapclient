8fe445e96b94d8d7104decee47917b5ce0e0c791
hsorby <h.sorby@auckland.ac.nz>
2022-12-20 13:14:17 +1300

    Rename more PySide2 to PySide6 mainly in tools.

--- src/mapclient/view/managers/package/ui/ui_packagemanagerdialog.py
+++ src/mapclient/view/managers/package/ui/ui_packagemanagerdialog.py
@@ -3,14 +3,23 @@
 ################################################################################
 ## Form generated from reading UI file 'packagemanagerdialog.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.2
+## Created by: Qt User Interface Compiler version 6.4.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtWidgets import *
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
+    QGroupBox, QHBoxLayout, QLabel, QListWidget,
+    QListWidgetItem, QPushButton, QSizePolicy, QSpacerItem,
+    QVBoxLayout, QWidget)
+import resources_rc
 
 class Ui_PackageManagerDialog(object):
     def setupUi(self, PackageManagerDialog):
