1b064862b430bc27bdecfa6eacba1ef56485d5ab
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-07-21 22:11:41 +1200

    Set windowed option for create application.

--- res/pyinstaller/hooks/hook-mapclientplugins.py
+++ res/pyinstaller/hooks/hook-mapclientplugins.py
@@ -5,7 +5,5 @@ import mapclientplugins
 
 # I don't think this will work if the plugin is zipped in an archive.
 hiddenimports = ['mapclientplugins']
-print("hook: mapclientplugins")
 for module_loader, name, ispkg in walk_packages(mapclientplugins.__path__, 'mapclientplugins.'):
     hiddenimports.append(name)
-    print("name:", name)
