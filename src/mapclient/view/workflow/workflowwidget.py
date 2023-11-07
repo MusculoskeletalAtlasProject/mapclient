4ef2187b94a2bebcb5454c0762940a168baf9c59
TimmyNic <tim.nicolas@outlook.com>
2023-11-07 16:06:07 +1300

    Fix import dialog ZIP identification
    This commit ensures both .proj and .zip files are visible and selectable when using the Import Workflow Configuration dialog. The current code is only recognising .proj files.

--- src/mapclient/view/workflow/workflowwidget.py
+++ src/mapclient/view/workflow/workflowwidget.py
@@ -597,7 +597,7 @@ class WorkflowWidget(QtWidgets.QWidget):
     def import_cfg(self):
         m = self._main_window.model().workflowManager()
         import_source, _ = QtWidgets.QFileDialog.getOpenFileName(self._main_window, caption='Select Import File', dir=m.previousLocation(),
-                                                                 filter=f"Data files(*.zip);MAP Client Project file({DEFAULT_WORKFLOW_PROJECT_FILENAME})")
+                                                                 filter=f"MAP Client Project file({DEFAULT_WORKFLOW_PROJECT_FILENAME} *.zip)")
 
         if len(import_source) > 0:
             dlg = ImportConfigDialog(import_source, self._graphicsScene, self)
