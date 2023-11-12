235993197ef6932e98677c55c96e7ab225f97082
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-11-13 09:46:37 +1300

    Add logger messages for debugging windows.

--- src/mapclient/application.py
+++ src/mapclient/application.py
@@ -273,8 +273,9 @@ def sans_gui_main(app_args):
     try:
         wm.scene().setMainWindow(FacadeMainWindow(model))
         wm.load(app_args.workflow)
-    except:
+    except Exception as e:
         logger.error('Not a valid workflow location: "{0}"'.format(app_args.workflow))
+        logger.error(f'error: {e}')
         sys.exit(INVALID_WORKFLOW_LOCATION_GIVEN)
 
     wm.registerDoneExecutionForAll(wm.execute)
