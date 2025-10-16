e90b86b732a22c5202d8680d14ee01861ed3e218
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-10-17 09:42:37 +1300

    Stop writing setup.py files in plugin wizard generated code, use requirements.txt instead.

--- src/mapclient/tools/pluginwizard/skeletonstrings.py
+++ src/mapclient/tools/pluginwizard/skeletonstrings.py
@@ -305,66 +305,6 @@ The {name} step is a plugin for the MAP Client application.
 """
 
 
-SETUP_PY_TEMPLATE = """\
-import codecs
-import io
-import os
-import re
-
-from setuptools import setup, find_packages
-
-SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
-def read(*parts):
-    with codecs.open(os.path.join(SETUP_DIR, *parts), 'r') as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\\"]([^'\\"]*)['\\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
-def readfile(filename, split=False):
-    with io.open(filename, encoding="utf-8") as stream:
-        if split:
-            return stream.read().split("\\n")
-        return stream.read()
-
-
-readme = readfile("README.rst", split=True)[3:]  # Skip title
-source_license = readfile("LICENSE")
-requires = ['PySide6']  # Minimal requirements listing. Insert additional dependencies here.
-
-
-setup(
-    name=%(name)r,
-    version=find_version(%(plugin_namespace)r, %(package_name)r, '__init__.py'),
-    description=%(description)r,
-    long_description='\\n'.join(readme) + source_license,
-    long_description_content_type='text/x-rst',
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python",
-    ],
-    author=%(author)r,
-    author_email=%(author_email)r,
-    url=%(url)r,
-    packages=find_packages(exclude=['ez_setup', ]),
-    namespace_packages=%(namespace_packages)r,
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=requires,
-)
-"""
-
-
 NAMESPACE_INIT = """\
 __import__('pkg_resources').declare_namespace(__name__)
 """
