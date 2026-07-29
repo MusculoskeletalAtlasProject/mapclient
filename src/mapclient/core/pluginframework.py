e6bbc180e9bcff16121ec5d5b503aa6cc6b2893b
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-07-29 15:58:31 +1200

    Try shifting the mocking of pkg resources so that spawned processes pick it up.

--- src/mapclient/core/pluginframework.py
+++ src/mapclient/core/pluginframework.py
@@ -177,27 +177,6 @@ def add_plugins(plugin_directories):
         sys.modules[PLUGINS_PACKAGE_NAME].__path__.append(os.path.join(d, PLUGINS_PACKAGE_NAME))
 
 def discover_plugins(plugin_directories):
-    # setuptools stopped supporting pkg_resources,
-    # but we cannot know if every plugin has removed
-    # this old import device. So, we will create a shim
-    # if it is no longer available and protect against
-    # plugins still using it.
-    mock_pkg_resources = False
-    try:
-        import pkg_resources
-        if not hasattr(pkg_resources, 'declare_namespace'):
-            mock_pkg_resources = True
-    except ModuleNotFoundError:
-        mock_pkg_resources = True
-
-    if mock_pkg_resources:
-        def declare_namespace(name):
-            pass
-
-        pkg_resources = types.ModuleType('pkg_resources')
-        pkg_resources.declare_namespace = declare_namespace
-        sys.modules['pkg_resources'] = pkg_resources
-
     try:
         importlib.import_module(PLUGINS_PACKAGE_NAME)
     except ModuleNotFoundError:
