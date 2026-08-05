98fec1e05a5c3441cc90022c79f6e2cdfa046cbd
Tim Salemink <82302710+tsalemink@users.noreply.github.com>
2026-08-05 12:20:13 +1200

    Add `mapclientplugins` hidden import

--- res/pyinstaller/create_application.py
+++ res/pyinstaller/create_application.py
@@ -58,6 +58,7 @@ def main(variant):
         '--hidden-import', 'scipy',
         '--hidden-import', 'scipy.interpolate',
         '--hidden-import', 'numpy',
+        '--hidden-import', 'mapclientplugins',
         '--additional-hooks-dir=hooks',
     ]
 
